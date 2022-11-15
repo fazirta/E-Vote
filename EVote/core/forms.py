1b924bafbecdc1ed3b2e259ecc53b66a66aa9529
yafi <yalif1710@gmail.com>
2022-11-15 21:01:18 +0700

    register model

--- EVote/core/forms.py
+++ EVote/core/forms.py
@@ -1,1 +1,9 @@
 from django import forms
+from . import models
+
+class Voting(forms):
+    class Meta:
+        model=models.Voting
+        fields=['paslon_1', 'paslon_2', 'paslon_3', 'paslon_4']
+
+#def ny gangerti lg
--- EVote/core/models.py
+++ EVote/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Voting(models.Model):
+    Paslon_1 = models.PositiveIntegerField(default=0)
+    Paslon_2 = models.PositiveIntegerField(default=0)
+    paslon_3 = models.PositiveIntegerField(default=0)
+    paslon_4 = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.Paslon_1
